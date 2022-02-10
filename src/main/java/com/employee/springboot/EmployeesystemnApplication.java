d49fc7cfb9e0918974e37cefc2ee99344fb512e3
Joan Vasquez <yoloprogramo22@gmail.com>
2022-02-10 11:48:54 -0400

    employee api

---
+++ src/main/java/com/employee/springboot/EmployeesystemnApplication.java
@@ -0,0 +1,13 @@
+package com.employee.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesystemnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesystemnApplication.class, args);
+	}
+
+}
